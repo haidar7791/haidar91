c10531e8801d9f656ef972edb160a568b990409c
Your Name <youremail@example.com>
2025-11-20 15:27:22 +0300

    Fix components: Map.js, Building.js, buildingsData.js, availableBuildings.js

---
+++ components/availableBuildings.js
@@ -0,0 +1,15 @@
+// components/availableBuildings.js
+const availableBuildings = [
+  "Town Hall_1",
+  "Cobalt_1",
+  "Cobalt warehouse_1",
+  "Mercury elixir_1",
+  "Elixir storehouse_1",
+  "Laser Tower_1",
+  "cannon_1",
+  "Forces camp_1",
+  "barracks_1",
+  "building hut"
+];
+
+export default availableBuildings;
