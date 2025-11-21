23bf7bc63149009f2c5b9ba91f508c437d84ac61
haidar7791 <mhymnalastwrh373@gmail.com>
2025-11-21 16:18:29 +0300

    Update Map.js

--- SpaceBaseGameBare/src/components/Map.js
+++ SpaceBaseGameBare/src/components/Map.js
@@ -41,7 +41,7 @@ const Map = () => {
     <View style={styles.container}>
       {/* أرضية اللعبة */}
       <Image
-        source={require("../../assets/images/game_floor.jpg")} // الاسم الفعلي للصورة
+        source={require("../../assets/images/Game_floor.jpg")} // الاسم الفعلي للصورة
         style={styles.game_Floor}
         resizeMode="stretch"
       />
